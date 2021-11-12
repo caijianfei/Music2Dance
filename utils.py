8e32dff650302d7753153f624d587d784b85a6d3
genify <jianfei.cai@mobilityasia.com.cn>
2021-11-12 16:58:19 +0800

    revert to support tensorflow 1.x

--- utils.py
+++ utils.py
@@ -16,13 +16,14 @@ import tensorflow as tf
 
 class Logger(object):
     def __init__(self, log_dir):
-        self.writer = tf.summary.create_file_writer(log_dir)
+        # self.writer = tf.summary.create_file_writer(log_dir)
+        self.writer = tf.summary.FileWriter(log_dir)
 
     def scalar_summary(self, tag, value, step):
-        tf.summary.scalar(tag, value.cpu(), step=step)
-        self.writer.flush()
-        # summary = tf.Summary(value=[tf.summary.Value(tag=tag, simple_value=value)])
-        # self.writer.add_summary(summary, step)
+        # tf.summary.scalar(tag, value, step=step)
+        # self.writer.flush()
+        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        self.writer.add_summary(summary, step)
 
 
 def vis(poses, outdir, aud=None):
