5681bc1df37e44b153d35cb9e2fa77ac50514dc6
genify <jianfei.cai@mobilityasia.com.cn>
2021-11-15 09:44:29 +0800

    format file

--- train_decomp.py
+++ train_decomp.py
@@ -18,47 +18,51 @@ from networks import *
 from options import DecompOptions
 from data import get_loader
 
+
 def getDecompNetworks(args):
-  initp_enc = InitPose_Enc(pose_size=args.pose_size, dim_z_init=args.dim_z_init)
-  initp_dec = InitPose_Dec(pose_size=args.pose_size, dim_z_init=args.dim_z_init)
-  movement_enc = Movement_Enc(pose_size=args.pose_size, dim_z_movement=args.dim_z_movement, length=args.stdp_length,
-                              hidden_size=args.movement_enc_hidden_size, num_layers=args.movement_enc_num_layers, bidirection=(args.movement_enc_bidirection==1))
-  stdp_dec = StandardPose_Dec(pose_size=args.pose_size, dim_z_movement=args.dim_z_movement, dim_z_init=args.dim_z_init, length=args.stdp_length,
-                             hidden_size=args.stdp_dec_hidden_size, num_layers=args.stdp_dec_num_layers)
-  return initp_enc, initp_dec, movement_enc, stdp_dec
+    initp_enc = InitPose_Enc(pose_size=args.pose_size, dim_z_init=args.dim_z_init)
+    initp_dec = InitPose_Dec(pose_size=args.pose_size, dim_z_init=args.dim_z_init)
+    movement_enc = Movement_Enc(pose_size=args.pose_size, dim_z_movement=args.dim_z_movement, length=args.stdp_length,
+                                hidden_size=args.movement_enc_hidden_size, num_layers=args.movement_enc_num_layers,
+                                bidirection=(args.movement_enc_bidirection == 1))
+    stdp_dec = StandardPose_Dec(pose_size=args.pose_size, dim_z_movement=args.dim_z_movement,
+                                dim_z_init=args.dim_z_init, length=args.stdp_length,
+                                hidden_size=args.stdp_dec_hidden_size, num_layers=args.stdp_dec_num_layers)
+    return initp_enc, initp_dec, movement_enc, stdp_dec
+
 
 if __name__ == "__main__":
-  parser = DecompOptions()
-  args = parser.parse()
+    parser = DecompOptions()
+    args = parser.parse()
 
-  args.train = True
+    args.train = True
 
-  if args.name is None:
-    args.name = 'Decomp'
+    if args.name is None:
+        args.name = 'Decomp'
 
-  args.log_dir = os.path.join(args.log_dir, args.name)
-  if not os.path.exists(args.log_dir):
-    os.mkdir(args.log_dir)
-  args.snapshot_dir = os.path.join(args.snapshot_dir, args.name)
-  if not os.path.exists(args.snapshot_dir):
-    os.mkdir(args.snapshot_dir)
+    args.log_dir = os.path.join(args.log_dir, args.name)
+    if not os.path.exists(args.log_dir):
+        os.mkdir(args.log_dir)
+    args.snapshot_dir = os.path.join(args.snapshot_dir, args.name)
+    if not os.path.exists(args.snapshot_dir):
+        os.mkdir(args.snapshot_dir)
 
-  data_loader = get_loader(batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, dataset=args.dataset, data_dir=args.data_dir, tolerance=args.tolerance)
+    data_loader = get_loader(batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers,
+                             dataset=args.dataset, data_dir=args.data_dir, tolerance=args.tolerance)
 
-  initp_enc, initp_dec, movement_enc, stdp_dec = getDecompNetworks(args)
+    initp_enc, initp_dec, movement_enc, stdp_dec = getDecompNetworks(args)
 
-  trainer = Trainer_Decomp(data_loader,
-                    initp_enc = initp_enc,
-                    initp_dec = initp_dec,
-                    movement_enc = movement_enc,
-                    stdp_dec = stdp_dec,
-                    args = args
-                    )
+    trainer = Trainer_Decomp(data_loader,
+                             initp_enc=initp_enc,
+                             initp_dec=initp_dec,
+                             movement_enc=movement_enc,
+                             stdp_dec=stdp_dec,
+                             args=args
+                             )
 
-  if not args.resume is None:
-    ep, it = trainer.resume(args.resume, False)
-  else:
-    ep, it = 0, 0
+    if not args.resume is None:
+        ep, it = trainer.resume(args.resume, False)
+    else:
+        ep, it = 0, 0
 
-  trainer.train(ep=ep, it=it)
-
+    trainer.train(ep=ep, it=it)
